eeed0bb22a213a7a3e4483230fdf9d41e6cb08a8
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2021-09-19 22:32:57 +0900

    changed hero from spiderman to batman

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import fetch from "isomorphic-unfetch";
 
 const Index = props => (
   <div>
-    <h1>Spiderman TV Shows</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
       {props.shows.map(show => (
         <li key={show.id}>
@@ -17,7 +17,7 @@ const Index = props => (
 );
 
 Index.getInitialProps = async function() {
-  const res = await fetch("https://api.tvmaze.com/search/shows?q=spiderman");
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
   const data = await res.json();
   console.log(`Show data fetched. Count: ${data.length}`);
   return { shows: data.map(entry => entry.show) };
